7f9d64ae713b1bff686d37e769cd3805de5e6fa4
atudosan <atudosan@yahoo.com>
2020-12-03 08:17:53 -0800

    added ConfigPropExtractData.class

---
+++ src/main/java/reusableComponents/ConfigPropExtractData.java
@@ -0,0 +1,23 @@
+package reusableComponents;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class ConfigPropExtractData {
+	static Properties prop = new Properties();
+	
+	public static String getPropValueByKey(String key) throws Exception {
+		String path = System.getProperty("user.dir")+"\\src\\test\\resources\\config.properties";
+		FileInputStream fis = new FileInputStream(path);
+		prop.load(fis);
+		String value = prop.get(key).toString();
+		
+		if(StringUtils.isEmpty(value)) {
+			throw new Exception("Value is not specified for key: "+key+" in config.properties file");
+		}
+		return value;		
+	}
+
+}
--- src/main/java/testBase/BrowserFactory.java
+++ src/main/java/testBase/BrowserFactory.java
@@ -2,8 +2,11 @@ package testBase;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -15,14 +18,27 @@ public class BrowserFactory {
 		WebDriver driver = null;
 
 		if (browser.equalsIgnoreCase("firefox")) {
+			
 			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-		} else if (browser.equalsIgnoreCase("edge")) {
-			driver = new FirefoxDriver();
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("--incognito");// to avoid cookies and cashes issues
+			driver = new ChromeDriver(options);
+			
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			
 			WebDriverManager.firefoxdriver().setup();
+			FirefoxOptions foptions = new FirefoxOptions();
+			foptions.addArguments("-private");
+			driver = new FirefoxDriver(foptions);
+			
+			
 		} else if (browser.equalsIgnoreCase("edge")) {
-			driver = new EdgeDriver();
+			
 			WebDriverManager.edgedriver().setup();
+			EdgeOptions eoptions = new EdgeOptions();
+			eoptions.addArguments("-private");
+			driver = new EdgeDriver(eoptions);
+			
 		}
 
 		return driver;
